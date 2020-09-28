bc948756078ae3eb8077cd776940991000b72404
Rodrigo <rodrigo.fo1990@gmail.com>
2020-09-28 04:32:16 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import Home from './component/Home';
+import MoviePage from './component/MoviePage';
+import popcornImg from './content/images/popcorn.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
 
-function App() {
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import MovieSearch from './component/MovieSearch';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/">
+              <img
+                alt=""
+                src={popcornImg}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{' '}RMovieDB</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/movies">Movie Search</Nav.Link>
+            </Nav>
+          </Navbar>
+          <Switch>
+            <Route path="/movie/:id" children={<MoviePage />} />
+            <Route path="/movies">
+              <MovieSearch />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    );
+  }
+}
+
+function Movies() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>ID: {id}</h3>
     </div>
   );
 }
